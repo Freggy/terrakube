4e46719b49d5803f77ed14a1970fb54483ab62e3
Yannic Rieger <riegeryannic@gmail.com>
2019-04-09 17:09:35 +0200

    Provision kube1 with ansible playbook

--- hcloud_kube1.tf
+++ hcloud_kube1.tf
@@ -2,6 +2,17 @@ resource "hcloud_server" "kube1" {
   name        = "kube1"
   image       = "debian-9"
   server_type = "cx11"
+  provisioner "remote-exec" {
+    inline = ["sudo apt-get install -y python"]
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = "${file(var.ssh_key)}"
+    }
+  } 
+  provisioner "local-exec" {
+    command = "ansible-playbook -u root -i ${hcloud_server.kube1.ipv4_address} --private_key ${var.ssh_key} docker-install.yml"
+  }
 }
 
 resource rke_cluster "cluster" {
