d6ec51d8081408bb1d59b0a662b3b372f73fca70
Yannic Rieger <riegeryannic@gmail.com>
2019-04-10 00:11:58 +0200

    One node k8s cluster now deploys successfully

--- hcloud_kube1.tf
+++ hcloud_kube1.tf
@@ -11,6 +11,20 @@ resource "hcloud_server" "kube1" {
     }
   } 
   provisioner "local-exec" {
-    command = "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -t docker -u root -i ${hcloud_server.kube1.ipv4_address}, --private-key ${var.ssh_key} docker-install.yml"
+   command = "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -t docker -u root -i ${hcloud_server.kube1.ipv4_address}, --private-key ${var.ssh_key} docker-install.yml"
   }
+}
+
+resource rke_cluster "cluster" {
+  nodes {
+      address = "${hcloud_server.kube1.ipv4_address}"
+      user    = "root"
+      role    = ["controlplane", "worker", "etcd"]
+      ssh_key = "${file(var.ssh_key)}"
+  }
+}
+
+resource "local_file" "kube_cluster_yaml" {
+  filename = "${path.root}/kube_config_cluster.yml"
+  content  = "${rke_cluster.cluster.kube_config_yaml}" 
 }
