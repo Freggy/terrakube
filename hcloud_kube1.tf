8074dda44e7181fa262aea6fb88e21a995ac569a
Yannic Rieger <riegeryannic@gmail.com>
2019-04-09 17:01:33 +0200

    Add rke plugin

--- hcloud_kube1.tf
+++ hcloud_kube1.tf
@@ -3,3 +3,11 @@ resource "hcloud_server" "kube1" {
   image       = "debian-9"
   server_type = "cx11"
 }
+
+resource rke_cluster "cluster" {
+  nodes {
+    address = "${hcloud_server.kube1.ipv4_address}"
+    user = "rancher"
+    role = ["controlplane", "worker", "etcd"]
+  }
+}
